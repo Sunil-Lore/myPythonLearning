8b38913bf1c8794ea71b4bcff5741deee4ef48cf
Sunil Kumar <sunil.lore@outlook.com>
2026-05-20 17:31:49 +0530

    indexing in string

---
+++ string/string006.py
@@ -0,0 +1,14 @@
+# string methods
+# indexing
+# string length
+# negative indexing
+#
+#
+a='Hello World'
+print(a[0])
+print(a[4])
+print(a[6])
+print(a[-1])
+print(a[-3])
+print(len(a))
+print(a[-11])
