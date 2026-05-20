e8143958a53e5aad09decb4d4bdcb69f2b740025
Sunil Kumar <sunil.lore@outlook.com>
2026-05-20 17:44:03 +0530

    escape sequences

---
+++ string/string009.py
@@ -0,0 +1,13 @@
+# Escape Sequences
+#
+# New line escape sequence
+print(" The BodyGuard\n is the best album" )
+
+# Tab escape sequence
+print(" The BodyGuard \t is the best album" )
+
+# Include back slash in string
+print(" The BodyGuard \\ is the best album" )
+
+# r will tell python that string will be display as raw string
+print(r" The BodyGuard \ is the best album" )
