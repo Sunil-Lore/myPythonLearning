a332737be43874489b6911a6ec100c2be788138c
Sunil Kumar <sunil.lore@outlook.com>
2026-05-20 16:57:13 +0530

    string method upper

--- basic/basic002.py
+++ basic/basic002.py
@@ -1,8 +1,10 @@
 #basic commands in Python
 # 1. use of backslash
 # 2. string concatenation
+# 3. Use of r before "..."
 
 print("Hello \n How are you?")
 fname="Sunil"
 lname="Kumar"
 print(fname +" "+ lname)
+print(r"I \ am")
---
+++ string003.py
@@ -0,0 +1,5 @@
+# String methods
+# upper()
+a="The quick brown fox jumps over the little lazy dog."
+b=a.upper()
+print(b)
