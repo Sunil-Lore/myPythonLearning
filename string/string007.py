143f3ab490e0745fba407547513094574edc0552
Sunil Kumar <sunil.lore@outlook.com>
2026-05-20 17:37:41 +0530

    slicing

---
+++ string/string007.py
@@ -0,0 +1,17 @@
+# String methods
+# Slicing
+# 
+#
+a='Hello World'
+# Take the slice on variable name with only index 0 to index 3
+print(a[0:4])
+
+# Take the slice on variable name with only index 8 to index 11
+print(a[8:15])
+
+# Get every second element, starting with the index 0. The elments on index 0, 2, 4 ...
+print(a[::2])
+
+# Get every second element in the range from index 0 to index 4
+
+print(a[0:5:2])
